743797ca3298899e65b0062398c7a3fee76d900b
JPLeon18 <juanpabloleonmera@gmail.com>
2018-09-16 20:09:26 -0500

    subida a github

---
+++ curso-angular4-webapp/src/app/components/productos-list.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ProductoService } from '../services/producto.service';
+import { Producto } from '../models/producto';
+import {getProdConfig} from '@angular/cli/models/webpack-configs';
+
+
+@Component({
+    selector: 'productos-list',
+    templateUrl: '../views/productos-list.html',
+    providers: [ProductoService]
+})
+
+export class ProductosListComponent{
+    public titulo: string;
+    public productos: Producto[];
+    public confirmado;
+
+    constructor(
+        private  _route: ActivatedRoute,
+        private  _router: Router,
+        private  _productoService: ProductoService
+    ){
+        this.titulo = 'Listado de productos';
+        this.confirmado = null;
+    }
+
+    ngOnInit(){
+        console.log('componente de productos-list.component.ts cargado');
+        this.getProductos();
+
+    }
+
+    getProductos(){
+        this._productoService.getProductos().subscribe(
+            result => {
+                if(result.code != 200){
+                    console.log(result);
+                }else{
+                    this.productos = result.data;
+                }
+            },
+            error => {
+                console.log(<any>error);
+            }
+        );
+    }
+
+
+
+    BorrarConfirm(id){
+        this.confirmado = id;
+    }
+
+    CancelarConfirm(){
+        this.confirmado = null;
+    }
+
+    onDeleteProducto(id){
+        this._productoService.deleteProducto(id).subscribe(
+            response => {
+
+                if(response.code == 200){
+                    this.getProductos();
+                }else{
+                    alert('Error al eliminar el producto');
+                }
+
+            },
+            error =>{
+                console.log(<any>error);
+            }
+        );
+    }
+
+}
